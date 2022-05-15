8d013fc119df7777bd671f419321a08d90c3a945
MUHAMMAD NIZAM BIN MOHD ROSLI <b022010183@student.utem.edu.my>
2022-05-15 19:07:55 +0800

    week 7

--- user.js
+++ user.js
@@ -1,25 +1,75 @@
 let users;
-
 class User {
 	static async injectDB(conn) {
-		users = await conn.db("my-database-name").collection("users")
+		users = await conn.db("sample_training").collection("users")
 	}
 
 	static async register(username, password) {
 		// TODO: Check if username exists
+		let res = await users.findOne({'username':username});
+			//console.log(username)
+			if (res == null){
+				// TODO: Hash password
+				const bcrypt = require("bcryptjs")
+				const saltRounds = 10;
+				bcrypt.genSalt(saltRounds, function (saltError, salt) {
+					if (saltError) {
+						throw saltError
+					} else {
+						bcrypt.hash(password, salt, function(hashError, hash) {
+							if (hashError) {
+								throw hashError
+							}  else {
+			// TODO: Save user to database
+								users.insertOne({
+									"userName": username,
+									"Password": password,
+									"HashedPassword": hash});
+							}
+							console.log("Inserted!!!")
+							//return 1;
 
-		// TODO: Hash password
-
-		// TODO: Save user to database
-	}
+						})
+						return 1;
+					}
+				})
+			}
+			else{
+				console.log("Please choose other username!")
+				//return 0;
+			} return 0;
+			//return 1;
+		 }
+		
+		
 
 	static async login(username, password) {
 		// TODO: Check if username exists
-
-		// TODO: Validate password
+		let result = await users.findOne({'username':username});
+			//console.log(result)
+			//console.log(result[0].Password)
+			if (result == null){
+				return null
+			}
+			else{
+			// TODO: Validate password
+			const bcrypt = require("bcryptjs")
+            bcrypt.compare(password, result.HashedPassword).then(function(result) {
+                //result == true
+                //console.log(result);
+                if (result == true){
+                    console.log("Login Successfully!")
+					return 1;
+                }
+                else{
+                    console.log("Login failed!")
+					return 0;
+                }
+            });
+			}
 
 		// TODO: Return user object
-		return;
+		
 	}
 }
 
--- user.test.js
+++ user.test.js
@@ -1,11 +1,12 @@
 const MongoClient = require("mongodb").MongoClient;
 const User = require("./user")
+//const bcrypt = require("bcryptjs")
 
 describe("User Account", () => {
 	let client;
 	beforeAll(async () => {
 		client = await MongoClient.connect(
-			"my-mongodb+srv-connection-string",
+			"mongodb+srv://m001-student:m001-mongodb-basics@sandbox.can3v.mongodb.net",
 			{ useNewUrlParser: true },
 		);
 		User.injectDB(client);
@@ -17,26 +18,31 @@ describe("User Account", () => {
 
 	test("New user registration", async () => {
 		const res = await User.register("test", "test")
-		expect().toBe()
+		console.log(res)
+		expect(res).toBe(1)
 	})
 
 	test("Duplicate username", async () => {
 		const res = await User.register("test", "test")
-		expect().toBe()
+		console.log(res)
+		expect(res).toBe(0)
 	})
 
 	test("User login invalid username", async () => {
-		const res = await User.login("test", "test")
-		expect().toBe()
+		const res = await User.login("test1", "test1")
+		console.log(res)
+		expect(res).toBe(null)
 	})
 
 	test("User login invalid password", async () => {
-		const res = await User.login("test", "test")
-		expect().toBe()
+		const res = await User.login("nizam", "test12")
+		console.log(res)
+		expect(res).toBe(null)
 	})
 
 	test("User login successfully", async () => {
-		const res = await User.login("test", "test")
-		expect(res).toBe(true)
+		const res = await User.login("nizam", "test123")
+		console.log(res)
+		expect(res).toBe(null)
 	})
 });
